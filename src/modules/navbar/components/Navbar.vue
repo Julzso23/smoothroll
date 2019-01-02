42fd44bedbc3fb116e77cbf11f985e9208436cfb
Julian Bath <julzso23@gmail.com>
2019-01-02 23:15:02 +0000

    Implemented navbar

--- src/modules/navbar/components/Navbar.vue
+++ src/modules/navbar/components/Navbar.vue
@@ -1,9 +1,25 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <navbar-link to="/">Dashboard</navbar-link>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
+  import NavbarLink from './NavbarLink';
+
   export default {
-    name: 'navbar'
+    name: 'navbar',
+    components: {
+      NavbarLink
+    }
   }
 </script>
 
