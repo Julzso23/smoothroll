de1c8d619966c3e92094d103490a7ee29b8afba2
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-02 23:15:02 +0000

    Implemented navbar

---
+++ src/modules/navbar/components/NavbarLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="nav-item">
+    <router-link :to="to" class="nav-link"><slot></slot></router-link>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'navbar-link',
+    props: {
+      to: String
+    }
+  }
+</script>
+
