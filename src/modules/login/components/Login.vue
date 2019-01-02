e2b37ea4e57e49404d708fef5017430679258a84
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-02 22:29:22 +0000

    Added login component

---
+++ src/modules/login/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent="login(account, password)">
+    <input v-model="account" type="text" name="account" placeholder="Username or email" class="form-control" />
+    <input v-model="password" type="password" name="password" placeholder="Password" class="form-control" />
+    <button type="submit" class="btn btn-primary">Login</button>
+  </form>
+</template>
+
+<script>
+  import login from 'mixins/login';
+
+  export default {
+    name: 'login',
+    data: () => ({
+      account: '',
+      password: ''
+    }),
+    mixins: [login]
+  }
+</script>
+
+<style lang="scss" module>
+</style>
