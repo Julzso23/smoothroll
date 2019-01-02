197ecbbfe8d086183957125596e858458c7489af
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-02 23:14:22 +0000

    Added router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
+  <main>
+    <navbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </main>
 </template>
 
 <script>
+  import Navbar from 'modules/navbar/components/Navbar';
+
   export default {
+    name: 'app',
+    components: {
+      Navbar
+    }
   }
 </script>
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Api from 'plugins/Api';
+import router from 'router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.use(Api);
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 });
