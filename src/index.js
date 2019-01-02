5ad376b5eafcfec2ccebb68214df69b068025568
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-02 22:29:56 +0000

    Installed API plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Api from 'plugins/Api';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.use(Api);
 
 new Vue({
   el: '#app',
