ffef37208d5cf0d4a84882107630434d9dea6652
Julian Bath <julzso23@gmail.com>
2019-01-03 16:23:25 +0000

    Moved login function to api plugin

--- src/modules/login/components/LoginForm.vue
+++ src/modules/login/components/LoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="login(account, password)">
+  <form @submit.prevent="$api.login(account, password)">
     <input v-model="account" type="text" name="account" placeholder="Username or email" class="form-control" />
     <input v-model="password" type="password" name="password" placeholder="Password" class="form-control" />
     <button type="submit" class="btn btn-primary">Login</button>
@@ -7,14 +7,11 @@
 </template>
 
 <script>
-  import login from '../mixins/login';
-
   export default {
     name: 'login-form',
     data: () => ({
       account: '',
       password: ''
-    }),
-    mixins: [login]
+    })
   }
 </script>
