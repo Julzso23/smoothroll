416efcf0b51e2686eed3713475196bd664a8c642
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-03 16:26:06 +0000

    Updated browse page to display results

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div class="row">
-      <dropdown-selector class="col-lg-3 col-md-4 col-sm-6" label="Filter" :options="filterOptions" @selectionUpdate="selection => filter = selection" />
-      <dropdown-selector class="col-lg-3 col-md-4 col-sm-6" label="Media" :options="mediaOptions" @selectionUpdate="selection => media = selection" />
+    <div class="row mb-4">
+      <dropdown-selector class="col-lg-3 col-md-4 col-sm-6" label="Filter" :options="filterOptions" @selectionUpdate="selection => {filter = selection; updateSeriesList()}" />
+      <dropdown-selector class="col-lg-3 col-md-4 col-sm-6" label="Media" :options="mediaOptions" @selectionUpdate="selection => {mediaType = selection; updateSeriesList()}" />
     </div>
+
+    <ul class="list-group">
+      <li class="list-group-item bg-dark text-light" v-for="series in seriesList" :key="series.series_id">{{series.name}}</li>
+    </ul>
   </div>
 </template>
 
@@ -30,7 +34,27 @@
         {key: 'anime', value: 'Anime'},
         {key: 'drama', value: 'Drama'}
       ],
-      media: 'anime'
-    })
+      mediaType: 'anime',
+
+      seriesList: [],
+
+      limit: 50,
+      offset: 0
+    }),
+    methods: {
+      async updateSeriesList() {
+        await this.$api.listSeries(this.filter, this.mediaType, this.limit, this.offset)
+          .then(data => this.seriesList = data)
+          .catch(code => {
+            if (code == 'bad_session') {
+              localStorage.removeItem('session_id');
+            }
+            this.$router.push('/login');
+          });
+      }
+    },
+    created() {
+      this.updateSeriesList();
+    }
   }
 </script>
