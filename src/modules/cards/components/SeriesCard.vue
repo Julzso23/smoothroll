3563c5c0f17fbf4bd409f66e6a67ede4835bfa69
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-04 13:39:21 +0000

    Added the series card component

---
+++ src/modules/cards/components/SeriesCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card bg-dark text-light">
+    <img class="card-img-top" :src="series.portrait_image.thumb_url" alt="Series Thumbnail" />
+    <div class="mx-2 my-1 truncate">
+      <span>{{series.name}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'series-card',
+    props: {
+      series: Object
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .truncate {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+</style>
