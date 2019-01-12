86996f822fce083f31114e207a5d58c59fe5ac81
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-12 19:55:56 +0000

    Added queue page and component

---
+++ src/pages/Queue.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <queue />
+  </div>
+</template>
+
+<script>
+  import Queue from 'modules/media/components/Queue';
+
+  export default {
+    name: 'queue-page',
+    components: {
+      Queue
+    }
+  }
+</script>
