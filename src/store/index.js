96e29cc05fcc4de4e47525c3ec204b40587543a5
Julian Bath <julzso23@gmail.com>
2019-01-12 19:21:57 +0000

    Added series store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import authentication from './authentication';
+import series from './series';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    authentication
+    authentication,
+    series
   }
 });
