6355e6bef4f2031de6dff59a519cf8e913621cdf
Julian Bath <julzso23@gmail.com>
2019-01-12 17:11:54 +0000

    Updated navbar authentication to use vuex

--- src/modules/navbar/components/Authentication.vue
+++ src/modules/navbar/components/Authentication.vue
@@ -1,31 +1,31 @@
 <template>
-  <ul class="navbar-nav" v-if="user == null">
-    <navbar-link to="/login">Login</navbar-link>
-  </ul>
-
-  <ul class="navbar-nav" v-else>
-    <span class="navbar-text text-muted">{{user.username}}</span>
+  <ul class="navbar-nav" v-if="isLoggedIn">
+    <span class="navbar-text text-muted">{{username}}</span>
     <li class="nav-item">
       <a class="nav-link" href="#" @click="$api.logout()">Logout</a>
     </li>
+  </ul>
+
+  <ul class="navbar-nav" v-else>
+    <navbar-link to="/login">Login</navbar-link>
   </ul>
 </template>
 
 <script>
   import NavbarLink from './NavbarLink';
-  import EventBus from 'EventBus';
 
   export default {
     name: 'navbar-authentication',
-    data: () => ({
-      user: null
-    }),
     components: {
       NavbarLink
     },
-    created() {
-      EventBus.$on('onLogin', data => this.user = data.user);
-      EventBus.$on('onLogout', () => this.user = null);
+    computed: {
+      isLoggedIn() {
+        return this.$store.getters.isLoggedIn;
+      },
+      username() {
+        return this.$store.state.authentication.user.username;
+      }
     }
   }
 </script>
