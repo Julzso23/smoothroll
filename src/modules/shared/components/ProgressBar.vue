59597b6a02b9b8ba94ab301e38d33b5f30fad429
Julian Bath <julzso23@gmail.com>
2019-01-13 22:02:21 +0000

    Added progress bar component

---
+++ src/modules/shared/components/ProgressBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="progress rounded-0" style="height: 4px">
+    <div class="progress-bar bg-primary" role="progressbar" :style="{width: value + '%'}" :aria-valuenow="value" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'progress-bar',
+    props: {
+      value: Number
+    }
+  }
+</script>
