584235811547f9937249f47a962d2d34cf616ed8
Julian Bath <Julzso23@users.noreply.github.com>
2019-01-31 15:58:01 +0000

    Added the queue to the dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,8 +1,17 @@
 <template>
+  <div>
+    <h3 class="text-light">Queue</h3>
+    <queue />
+  </div>
 </template>
 
 <script>
+  import Queue from 'modules/media/components/Queue';
+
   export default {
-    name: 'dashboard'
+    name: 'dashboard',
+    components: {
+      Queue
+    }
   }
 </script>
