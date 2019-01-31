abd74dc5d52ffb820dc52ce8e3199c89007e0a83
Julian Bath <julzso23@gmail.com>
2019-01-31 22:11:28 +0000

    Added series portrait image

--- src/pages/Series.vue
+++ src/pages/Series.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <h3 class="text-light">{{series.name}}</h3>
-    <p class="text-light">{{series.description}}</p>
+    <div class="row">
+      <div class="col-9">
+        <h3 class="text-light">{{series.name}}</h3>
+        <p class="text-light">{{series.description}}</p>
+      </div>
+
+      <div class="col-3">
+        <img class="img-fluid mx-auto d-block" v-if="series.portrait_image" :src="series.portrait_image.large_url" />
+      </div>
+    </div>
   </div>
 </template>
 
