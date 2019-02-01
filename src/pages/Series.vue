47cf959decb781fe85d8c76c57852961fd9ffa8e
Julian Bath <julzso23@gmail.com>
2019-02-01 17:36:28 +0000

    Moved collection listing to its own component

--- src/pages/Series.vue
+++ src/pages/Series.vue
@@ -11,22 +11,12 @@
       </div>
     </div>
 
-    <div v-for="collection in mediaCollections" :key="collection.id" class="mb-2">
-      <button class="btn btn-block btn-secondary text-left" type="button" data-toggle="collapse" :data-target="`#collection-${collection.id}`" aria-expanded="false" :aria-controls="`collection-${collection.id}`">
-        {{collection.name}}
-        <span class="dropdown-toggle float-right"></span>
-      </button>
-      <div class="row collapse mt-2" :id="`collection-${collection.id}`">
-        <div class="col-lg-3 col-md-4 col-sm-6 offset-sm-0 col-8 offset-2 mb-4" v-for="mediaItem in collection.media" :key="mediaItem.media_id">
-          <media-card :media="mediaItem" />
-        </div>
-      </div>
-    </div>
+    <collection v-for="collection in mediaCollections" :key="collection.id" :collection="collection" />
   </div>
 </template>
 
 <script>
-  import MediaCard from 'modules/cards/components/MediaCard';
+  import Collection from 'modules/media/components/Collection';
 
   export default {
     name: 'series',
@@ -54,7 +44,7 @@
       }
     },
     components: {
-      MediaCard
+      Collection
     },
     watch: {
       series: function(value) {
