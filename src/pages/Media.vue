31e0307effb883b20e23e0730d46b775fea09863
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-06 15:03:09 +0000

    Added loading component to media page

--- src/pages/Media.vue
+++ src/pages/Media.vue
@@ -1,15 +1,17 @@
 <template>
-  <div v-if="media">
+  <div v-if="media && media.media_id == mediaId">
     <player :streamData="media.stream_data" :poster="media.screenshot_image.full_url" />
 
     <h3 class="text-light"><router-link :to="'/series/' + media.series_id" class="text-reset">{{media.series_name}}</router-link></h3>
     <h4 class="text-light">{{'Episode ' + media.episode_number + ' - ' + media.name}}</h4>
     <p class="text-light">{{media.description}}</p>
   </div>
+  <loading v-else />
 </template>
 
 <script>
   import Player from 'modules/media/components/Player';
+  import Loading from 'modules/shared/components/Loading';
 
   export default {
     name: 'media',
@@ -25,7 +27,8 @@
       }
     },
     components: {
-      Player
+      Player,
+      Loading
     },
     watch: {
       mediaId() {
