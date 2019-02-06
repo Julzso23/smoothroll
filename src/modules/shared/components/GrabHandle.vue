d842ec1e1e680aae2d6c64c22bb170c3d8c65eaf
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-06 19:21:13 +0000

    Added grab handle component for drawing an array of dots

---
+++ src/modules/shared/components/GrabHandle.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="grab-handle" :style="{width: dotsX * 6 + 'px', height: dotsY * 6 + 'px'}"></div>
+</template>
+
+<script>
+  export default {
+    name: 'grab-handle',
+    props: {
+      dotsX: Number,
+      dotsY: Number
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @import 'scss/_variables';
+  $bg-color: $dark;
+  $dot-color: #eee;
+  $dot-size: 2px;
+  $dot-space: 6px;
+
+  .grab-handle {
+    margin: auto;
+    background:
+      linear-gradient(90deg, $bg-color ($dot-space - $dot-size), transparent 1%) center,
+      linear-gradient($bg-color ($dot-space - $dot-size), transparent 1%) center, $dot-color;
+    background-size: $dot-space $dot-space;
+  }
+</style>
