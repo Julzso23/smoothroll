29ccd3c7622082ccc8284dce7fc866e062a09230
Julian Bath <julzso23@gmail.com>
2019-02-08 12:43:50 +0000

    Added ribbons to cards for series in the queue

--- src/modules/cards/components/SeriesCard.vue
+++ src/modules/cards/components/SeriesCard.vue
@@ -1,8 +1,11 @@
 <template>
   <card>
+    <ribbon v-if="series.in_queue" />
+
     <router-link :to="'/series/' + series.series_id" class="text-reset">
       <img class="card-img-top" :src="series.portrait_image.thumb_url" alt="Series Thumbnail" />
     </router-link>
+
     <div class="mx-2 my-1 text-truncate">
       <span><router-link :to="'/series/' + series.series_id" class="text-reset">{{series.name}}</router-link></span>
     </div>
@@ -11,6 +14,7 @@
 
 <script>
   import Card from './Card';
+  import Ribbon from './Ribbon';
 
   export default {
     name: 'series-card',
@@ -18,7 +22,8 @@
       series: Object
     },
     components: {
-      Card
+      Card,
+      Ribbon
     }
   }
 </script>
