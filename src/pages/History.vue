a66636ae1762f25b7ee25175f5cf26160b9ee147
Julian Bath <julzso23@gmail.com>
2019-02-08 13:59:27 +0000

    Added history loading screen

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -4,16 +4,19 @@
       <dropdown-selector class="col-lg-3 col-md-4 col-sm-6 mb-2" label="Media" :options="mediaOptions" @selectionUpdate="selection => {mediaType = selection; updateMediaList()}" />
     </div>
 
-    <div class="row">
+    <div class="row" v-if="!loading">
       <div class="col-lg-3 col-md-4 col-sm-6 offset-sm-0 col-8 offset-2 mb-4" v-for="media in mediaList" :key="media.media_id">
         <media-card :media="media" />
       </div>
     </div>
+
+    <loading v-else />
   </div>
 </template>
 
 <script>
   import DropdownSelector from 'modules/shared/components/DropdownSelector';
+  import Loading from 'modules/shared/components/Loading';
   import MediaCard from 'modules/cards/components/MediaCard';
 
   export default {
@@ -27,15 +30,22 @@
       mediaType: 'anime',
 
       limit: 50,
-      offset: 0
+      offset: 0,
+
+      loading: false
     }),
     methods: {
       async updateMediaList() {
+        this.loading = true;
+
         await this.$store.dispatch('getHistory', {
           mediaTypes: this.mediaType,
           limit: this.limit,
           offset: this.offset
-        });
+        })
+          .then(() => {
+            this.loading = false;
+          });
       }
     },
     created() {
@@ -44,7 +54,8 @@
     },
     components: {
       DropdownSelector,
-      MediaCard
+      MediaCard,
+      Loading
     },
     computed: {
       mediaList() {
