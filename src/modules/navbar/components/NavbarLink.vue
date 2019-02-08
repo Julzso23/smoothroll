78017ec4916d2b262153326b6b3379fc8eacac17
Julian Bath <julzso23@gmail.com>
2019-02-08 12:26:43 +0000

    Added active navbar link underline

--- src/modules/navbar/components/NavbarLink.vue
+++ src/modules/navbar/components/NavbarLink.vue
@@ -12,3 +12,15 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import 'scss/_variables';
+
+  .nav-item > .nav-link {
+    padding: 12px;
+  }
+
+  .router-link-exact-active {
+    border-bottom: solid 2px $primary;
+  }
+</style>
