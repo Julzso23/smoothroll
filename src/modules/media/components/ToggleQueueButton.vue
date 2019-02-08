8a6632c64884e49c9be3747a5ff17acf916ffd8f
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-08 14:13:06 +0000

    Added toggle queue button with loading animation

---
+++ src/modules/media/components/ToggleQueueButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="btn btn-primary" @click="toggleQueue" v-if="!loading">{{inQueue ? 'Remove from queue' : 'Add to queue'}}</button>
+  <button class="btn btn-disabled" v-else><loading /></button>
+</template>
+
+<script>
+  import Loading from 'modules/shared/components/Loading';
+
+  export default {
+    name: 'toggle-queue-button',
+    props: {
+      seriesId: String,
+      inQueue: Boolean
+    },
+    data: () => ({
+      loading: false
+    }),
+    methods: {
+      toggleQueue() {
+        this.loading = true;
+
+        this.$store.dispatch('toggleQueue', {
+          seriesId: this.seriesId,
+          inQueue: this.inQueue
+        })
+          .then(() => {
+            this.$emit('toggle', this.endLoading);
+          });
+      },
+      endLoading() {
+        this.loading = false;
+      }
+    },
+    components: {
+      Loading
+    }
+  }
+</script>
