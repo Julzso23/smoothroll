2cef5e276109940052a6e50ad78120e09bb004cc
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-08 12:27:11 +0000

    Added card component

---
+++ src/modules/cards/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card bg-dark text-light">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'card'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import 'scss/_variables';
+
+  .card {
+    border: none;
+  }
+</style>
--- src/modules/cards/components/SeriesCard.vue
+++ src/modules/cards/components/SeriesCard.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="card bg-dark text-light">
+  <card>
     <router-link :to="'/series/' + series.series_id" class="text-reset">
       <img class="card-img-top" :src="series.portrait_image.thumb_url" alt="Series Thumbnail" />
     </router-link>
     <div class="mx-2 my-1 text-truncate">
       <span><router-link :to="'/series/' + series.series_id" class="text-reset">{{series.name}}</router-link></span>
     </div>
-  </div>
+  </card>
 </template>
 
 <script>
+  import Card from './Card';
+
   export default {
     name: 'series-card',
     props: {
       series: Object
+    },
+    components: {
+      Card
     }
   }
 </script>
