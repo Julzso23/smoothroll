4f2d26fadbc9b7248f601ef973a9aa76fae6fa51
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-11 15:11:02 +0000

    Added a footer with a link to the github repo and some disclaimers

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <main>
     <navbar />
+
     <div class="container">
       <router-view />
     </div>
+
+    <app-footer />
   </main>
 </template>
 
 <script>
   import Navbar from 'modules/navbar/components/Navbar';
+  import AppFooter from 'modules/footer/components/Footer';
 
   export default {
     name: 'app',
     components: {
-      Navbar
+      Navbar,
+      AppFooter
     }
   }
 </script>
---
+++ src/modules/footer/components/FooterItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-light col-md-6 my-2">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'footer-item'
+  }
+</script>
