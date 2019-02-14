e15f431de148b5a3b9cb7c16fe89cecf59710fa6
Julian Bath <julzso23@gmail.com>
2019-02-14 10:46:14 +0000

    Added 404 page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-light">404 - Page not found</h1>
+    <p class="text-light">It seems you've tried to access a page that doesn't exist.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'not-found'
+  }
+</script>
