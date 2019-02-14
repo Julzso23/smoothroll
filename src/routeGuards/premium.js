215b69cfa2bc9506c935ea0be53ec4a9fe231295
Julian Bath <julzso23@gmail.com>
2019-02-14 10:42:56 +0000

    Refactored beforeEach function into separate guard functions

---
+++ src/routeGuards/premium.js
@@ -0,0 +1,13 @@
+import store from 'store';
+
+export default function(to, from, next) {
+  if (store.getters.isLoggedIn) {
+    if (store.getters.isPremium) {
+      next();
+    } else {
+      next('premium');
+    }
+  } else {
+    next('login');
+  }
+};
