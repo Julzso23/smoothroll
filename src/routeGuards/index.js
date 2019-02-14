774ba4f9963fd1b093526034a63aab7f06f5afa5
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-14 10:42:56 +0000

    Refactored beforeEach function into separate guard functions

---
+++ src/routeGuards/index.js
@@ -0,0 +1,7 @@
+import auth from './auth';
+import premium from './premium';
+import login from './login';
+
+export const authGuard = auth;
+export const premiumGuard = premium;
+export const loginGuard = login;
