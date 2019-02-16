136b09c1b8869ff98045224be1d13ea956fe47ee
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-16 22:51:02 +0000

    Added spanish language strings

--- src/locale/index.js
+++ src/locale/index.js
@@ -3,6 +3,8 @@ import VueI18n from 'vue-i18n';
 import store from 'store';
 
 import enUS from './enUS';
+import esLA from './esLA';
+import esES from './esES';
 
 Vue.use(VueI18n);
 
@@ -10,7 +12,9 @@ export default new VueI18n({
   locale: store.state.locale.locale,
   fallbackLocale: 'enUS',
   messages: {
-    enUS
+    enUS,
+    esLA,
+    esES
   },
   silentTranslationWarn: true
 });
