216c27c14e817e2b8af610586e34062a6bb6e724
Julian Bath <Julzso23@users.noreply.github.com>
2019-02-16 23:32:43 +0000

    Added italian language strings

---
+++ src/locale/itIT/media.js
@@ -0,0 +1,21 @@
+export default {
+  episode: 'Episodio {number}',
+  queueAdd: 'Aggiungi alla coda',
+  queueRemove: 'Rimuovi dalla coda',
+  filter: 'Filtro',
+  media: 'Media',
+  loadMore: 'Carica di più',
+  types: {
+    anime: 'Anime',
+    drama: 'Dramma',
+    both: 'Tutti e due'
+  },
+  filters: {
+    alphabetical: 'Alfabetico',
+    featured: 'In primo piano',
+    newest: 'I più nuovi',
+    popular: 'Popolare',
+    simulcast: 'Simulcast',
+    updated: 'Aggiornato'
+  }
+};
