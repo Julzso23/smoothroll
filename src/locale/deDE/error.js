373479d45f4b92d8e7b9204f20598b3e5cd35993
Julian Bath <julzso23@gmail.com>
2019-02-16 23:23:45 +0000

    Fixed formatting in some language strings

--- src/locale/deDE/error.js
+++ src/locale/deDE/error.js
@@ -1,5 +1,5 @@
 export default {
-  notFound: '404 Seite nicht gefunden',
+  notFound: '404 - Seite nicht gefunden',
   notFoundLong: 'Es scheint, dass Sie versucht haben, auf eine Seite zuzugreifen, die nicht existiert.',
   premium: 'Sie benötigen ein Premium-Konto, um diesen Service nutzen zu können!'
 }
