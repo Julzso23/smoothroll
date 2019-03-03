9ddf5c5f092493c46349f7e9c17db894b9f05203
Julian Bath <Julzso23@users.noreply.github.com>
2019-03-03 19:20:52 +0000

    Updated premium error message to specify Crunchyroll premium

--- src/locale/deDE/error.js
+++ src/locale/deDE/error.js
@@ -1,5 +1,5 @@
 export default {
   notFound: '404 - Seite nicht gefunden',
   notFoundLong: 'Es scheint, dass Sie versucht haben, auf eine Seite zuzugreifen, die nicht existiert.',
-  premium: 'Sie benötigen ein Premium-Konto, um diesen Service nutzen zu können!'
+  premium: 'Sie benötigen ein Crunchyroll-Premium-Konto, um diesen Service nutzen zu können!'
 }
--- src/locale/enUS/error.js
+++ src/locale/enUS/error.js
@@ -1,5 +1,5 @@
 export default {
   notFound: '404 - Page not found',
   notFoundLong: 'It seems you\'ve tried to access a page that doesn\'t exist.',
-  premium: 'You need to have a premium account to use this service!'
+  premium: 'You need to have a Crunchyroll premium account to use this service!'
 }
