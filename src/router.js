4ade3a2929a3d5c8852bf04e9d751b37b5b496ae
Jamie Simmonds <jamie.simmonds96@gmail.com>
2019-03-03 19:40:12 +0000

    Added: Babel ESLint and ESLint for StandardJS

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import {authGuard, premiumGuard, loginGuard} from 'routeGuards';
+import Vue from 'vue'
+import Router from 'vue-router'
+import { authGuard, premiumGuard, loginGuard } from 'routeGuards'
 
-Vue.use(Router);
+Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
@@ -87,14 +87,14 @@ const router = new Router({
       component: () => import('pages/NotFound')
     }
   ]
-});
+})
 
 router.beforeEach((to, from, next) => {
   if (to.meta.guard != null) {
     to.meta.guard(to, from, next)
   } else {
-    next();
+    next()
   }
-});
+})
 
-export default router;
+export default router
