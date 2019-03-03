15ec3369fd0d173c9d6eb50b4abcb451bd84e5da
Julian Bath <julzso23@gmail.com>
2019-03-03 16:15:51 +0000

    Fixed logo path

--- src/modules/navbar/components/Navbar.vue
+++ src/modules/navbar/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="bg-dark mb-4">
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <router-link to="/" class="navbar-brand"><img src="logo.svg" alt="logo" /></router-link>
+        <router-link to="/" class="navbar-brand"><img src="/logo.svg" alt="logo" /></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
