2be560d296981ce2ea3fbfa468c653a1d920b0bd
Julian Bath <julzso23@gmail.com>
2019-03-04 17:15:58 +0000

    Moved shared browse functionality to the series list component and added the tags browse page

---
+++ src/modules/media/components/SeriesList.vue
@@ -0,0 +1,98 @@
+<template>
+  <div v-if="!loading" class="mb-4">
+    <div class="alert alert-info bg-dark text-light" v-if="!seriesList.length">
+      {{$t('media.emptyBrowse')}}
+    </div>
+
+    <div class="row">
+      <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4" v-for="series in seriesList" :key="series.series_id">
+        <series-card :series="series" />
+      </div>
+    </div>
+
+    <div v-if="canLoadMore">
+      <button class="btn btn-block btn-primary" v-if="!loadingMore" @click="loadMore">{{$t('media.loadMore')}}</button>
+      <button class="btn btn-block btn-disabled" v-else><loading /></button>
+    </div>
+  </div>
+
+  <loading v-else />
+</template>
+
+<script>
+  import Loading from 'modules/shared/components/Loading';
+  import SeriesCard from 'modules/cards/components/SeriesCard';
+
+  export default {
+    name: 'series-list',
+    components: {
+      SeriesCard,
+      Loading
+    },
+    props: {
+      filter: String,
+      mediaType: String
+    },
+    data: function() {
+      return {
+        limit: 50,
+        offset: 0,
+
+        loading: false,
+        loadingMore: false,
+        canLoadMore: false
+      };
+    },
+    computed: {
+      seriesList() {
+        return this.$store.state.browse.seriesList;
+      }
+    },
+    methods: {
+      async updateSeriesList() {
+        this.loading = true;
+        this.offset = 0;
+
+        await this.$store.dispatch('listSeries', {
+          filter: this.filter,
+          mediaType: this.mediaType,
+          limit: this.limit,
+          offset: this.offset
+        })
+          .then(data => {
+            this.loading = false;
+            if (this.seriesList.length % this.limit != 0 || data.length == 0) {
+              this.canLoadMore = false;
+            }
+          });
+      },
+
+      async loadMore() {
+        this.loadingMore = true;
+        this.offset += this.limit;
+
+        await this.$store.dispatch('listSeries', {
+          filter: this.filter,
+          mediaType: this.mediaType,
+          limit: this.limit,
+          offset: this.offset,
+          append: true
+        })
+          .then(data => {
+            this.loadingMore = false;
+            if (this.seriesList.length % this.limit != 0 || data.length == 0) {
+              this.canLoadMore = false;
+            }
+          });
+      }
+    },
+    watch: {
+      filter() {
+        this.updateSeriesList();
+      },
+      mediaType() {
+        this.updateSeriesList();
+      }
+    }
+  }
+</script>
--- src/router.js
+++ src/router.js
@@ -37,7 +37,7 @@ const router = new Router({
         },
         {
           path: 'categories/:mediaType/:tag',
-          component: () => import('pages/browse/Filter')
+          component: () => import('pages/browse/Tags')
         },
         {
           path: '',
