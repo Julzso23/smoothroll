17f8da74831cce584f5e974af2da59bd2e380e48
Julian Bath <Julzso23@users.noreply.github.com>
2019-03-17 20:29:04 +0000

    Added compact series card

---
+++ src/modules/cards/components/CompactSeriesCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :to="'/series/' + series.series_id" :title="series.name">
+    <card>
+      <div class="mx-4 my-2 text-truncate">
+        {{series.name}}
+      </div>
+    </card>
+  </router-link>
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+  name: 'compact-series-card',
+  props: {
+    series: {
+      series_id: String,
+      name: String
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
