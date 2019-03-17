d2946940390d7d6db14a69fae5f8db028e8ed40a
Julian Bath <Julzso23@users.noreply.github.com>
2019-03-17 20:50:12 +0000

    Updated dropdown selector to use v-model

--- src/modules/shared/components/DropdownSelector.vue
+++ src/modules/shared/components/DropdownSelector.vue
@@ -3,7 +3,7 @@
     <div class="input-group-prepend">
       <label class="input-group-text bg-dark text-light">{{label}}</label>
     </div>
-    <select class="custom-select" v-model="selection" @change="onChange">
+    <select class="custom-select" :value="value ? value.value : ''" @change="onChange">
       <option v-for="option in options" :key="option.key">{{option.value}}</option>
     </select>
   </div>
@@ -15,32 +15,19 @@ export default {
   props: {
     options: Array,
     label: String,
-    initialSelection: String
-  },
-  data: () => ({
-    selection: ''
-  }),
-  mounted () {
-    if (this.initialSelection) {
-      for (let option of this.options) {
-        if (option.key === this.initialSelection) {
-          this.selection = option.value
-          return
-        }
-      }
+    value: {
+      key: String,
+      value: String
     }
-    this.selection = this.options[0].value
   },
   methods: {
-    onChange () {
-      let key = ''
+    onChange (event) {
       for (let option of this.options) {
-        if (option.value === this.selection) {
-          key = option.key
+        if (option.value === event.target.value) {
+          this.$emit('input', option)
           break
         }
       }
-      this.$emit('selectionUpdate', key)
     }
   }
 }
