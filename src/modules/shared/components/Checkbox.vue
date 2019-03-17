ad2b431009aaa6ff1e14a474cc124ab8d0579746
Julian Bath <julzso23@gmail.com>
2019-03-17 20:29:41 +0000

    Added checkbox component

---
+++ src/modules/shared/components/Checkbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="input-group">
+    <div class="input-group-prepend">
+      <label class="input-group-text bg-dark text-light">Compact</label>
+    </div>
+    <div class="input-group-append">
+      <div class="input-group-text">
+        <input type="checkbox" :checked="value" @input="input" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'checkbox',
+  props: {
+    value: Boolean
+  },
+  methods: {
+    input (event) {
+      this.$emit('input', event.target.checked)
+    }
+  }
+}
+</script>
