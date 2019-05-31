51cc2585ad0c857b1d3dc932d4bd260a4fa2f143
Julian Bath <Julzso23@users.noreply.github.com>
2019-05-31 10:41:55 +0100

    Removed import for unused route guard

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import { authGuard, premiumGuard, nonPremiumGuard, loginGuard } from 'routeGuards'
+import { premiumGuard, nonPremiumGuard, loginGuard } from 'routeGuards'
 
 Vue.use(Router)
 
