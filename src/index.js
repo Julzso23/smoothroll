5031de041ad4da836d68db425ec519ae0eff1cd0
Julian Bath <julzso23@gmail.com>
2019-05-31 13:17:40 +0100

    Added Google analytics

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import MixedContent from 'plugins/MixedContent'
 import router from 'router'
 import store from 'store'
 import i18n from 'locale'
+import VueAnalytics from 'vue-analytics'
 import 'bootstrap'
 import 'scss/_theme'
 
@@ -13,6 +14,19 @@ Vue.use(MixedContent)
 
 Vue.create = function (options) {
   return new Vue(options)
+}
+
+if (process.env.GA_KEY) {
+  Vue.use(VueAnalytics, {
+    id: process.env.GA_KEY,
+    router,
+    set: [
+      {
+        field: 'anonymizeIp',
+        value: true
+      }
+    ]
+  })
 }
 
 Vue.create({
