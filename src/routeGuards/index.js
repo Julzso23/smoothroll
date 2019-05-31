976598bf1d7a839d6fe157395c21ab2a0d350ddd
Julian Bath <julzso23@gmail.com>
2019-05-31 10:38:09 +0100

    Fixed route guards for premium and browse pages

--- src/routeGuards/index.js
+++ src/routeGuards/index.js
@@ -1,7 +1,9 @@
 import auth from './auth'
 import premium from './premium'
+import nonPremium from './nonPremium'
 import login from './login'
 
 export const authGuard = auth
 export const premiumGuard = premium
+export const nonPremiumGuard = nonPremium
 export const loginGuard = login
