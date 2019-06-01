1a320035f9204edadf336396a93125308c83fb9b
Julian Bath <julzso23@gmail.com>
2019-06-01 14:15:19 +0100

    Updated routes to add landing page

---
+++ src/routeGuards/landing.js
@@ -0,0 +1,9 @@
+import store from 'store'
+
+export default function (to, from, next) {
+  if (!store.getters['authentication/isLoggedIn']) {
+    next()
+  } else {
+    next('/dashboard')
+  }
+};
