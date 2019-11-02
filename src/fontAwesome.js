4af766cd0ea7018e951eec6db4a9c17a963c1aa0
Jamie Simmonds <jamie.simmonds96@gmail.com>
2019-11-02 05:59:47 +0000

    Added: Error icon to video player if streams is empty (╯°□°)╯︵ ┻━┻

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronLeft, faChevronRight, faEye } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faEye, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faChevronLeft)
 library.add(faChevronRight)
 library.add(faEye)
+library.add(faExclamationTriangle)
 
 Vue.component('fa-icon', FontAwesomeIcon)
