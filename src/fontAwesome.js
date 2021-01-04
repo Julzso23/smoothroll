09ae7f0921d2e0f690b8c6cab76abc92e160c5f9
Julian <Julzso23@users.noreply.github.com>
2021-01-04 14:16:45 +0000

    Added icons to navbar buttons

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronLeft, faChevronRight, faEye, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faEye, faExclamationTriangle, faHistory, faTasks, faSignOutAlt, faFolderOpen, faStream } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faChevronLeft)
-library.add(faChevronRight)
-library.add(faEye)
-library.add(faExclamationTriangle)
+library.add(faChevronLeft, faChevronRight, faEye, faExclamationTriangle, faHistory, faTasks, faSignOutAlt, faFolderOpen, faStream)
 
 Vue.component('fa-icon', FontAwesomeIcon)
